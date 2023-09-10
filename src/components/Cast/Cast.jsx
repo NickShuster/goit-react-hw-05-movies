919c9499800adae2b24b63193c93cd83a6f82894
NickShuster <124299055+NickShuster@users.noreply.github.com>
2023-09-10 23:08:47 +0200

    коміттт

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,32 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchMovieCredits } from '../api';
+import { fetchMovieCredits, fetchActorImages } from '../api';
 
 function Cast() {
   const { movieId } = useParams();
   const [castData, setCastData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const cast = await fetchMovieCredits(movieId);
-        setCastData(cast);
+        const promises = cast.map(async (actor) => {
+          try {
+            const actorImages = await fetchActorImages(actor.id);
+            return { ...actor, images: actorImages };
+          } catch (error) {
+            console.error('Помилка під час завантаження фотографій актора:', error);
+            return actor; 
+          }
+        });
+        const castWithImages = await Promise.all(promises);
+        setCastData(castWithImages);
+        setLoading(false);
       } catch (error) {
         console.error('Помилка під час завантаження даних акторів:', error);
+        setLoading(false);
       }
     };
 
     fetchData();
   }, [movieId]);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h2>Cast</h2>
       <ul>
         {castData.map((actor) => (
           <li key={actor.id}>
             <p>Name: {actor.name}</p>
             <p>Character: {actor.character}</p>
+            {actor.images && actor.images.length > 0 && (
+              <div>
+                <img src={`https://image.tmdb.org/t/p/w200/${actor.images[0].file_path}`} alt={actor.name} />
+              </div>
+            )}
           </li>
         ))}
       </ul>
--- src/components/api.js
+++ src/components/api.js
@@ -1,81 +1,3 @@
-//  import axios from 'axios';
-
-// const API_KEY = '4f40632f604dfec179dda8e530bb62da';
-// const BASE_URL = 'https://api.themoviedb.org/3';
-
-// const fetchTrendingMovies = async () => {
-//   try {
-//     const response = await axios.get(
-//       `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`
-//     );
-//     return response.data.results;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// const searchMovies = async (query) => {
-//   try {
-//     const response = await axios.get(
-//       `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
-//     );
-//     return response.data.results;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// const fetchMovieDetails = async (movieId) => {
-//   try {
-//     const response = await axios.get(
-//       `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`
-//     );
-//     return response.data;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// const fetchMovieCredits = async (movieId) => {
-//   try {
-//     const response = await axios.get(
-//       `${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`
-//     );
-//     return response.data.cast;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// const fetchMovieReviews = async (movieId) => {
-//   try {
-//     const response = await axios.get(
-//       `${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}`
-//     );
-//     return response.data.results;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// export {
-//   fetchTrendingMovies,
-//   searchMovies,
-//   fetchMovieDetails,
-//   fetchMovieCredits,
-//   fetchMovieReviews,
-// };
-
-
-
-
-
-
-
-
-
-
-
 import axios from 'axios';
 
 const API_KEY = '4f40632f604dfec179dda8e530bb62da';
@@ -125,6 +47,17 @@ const fetchMovieCredits = async (movieId) => {
   }
 };
 
+const fetchActorImages = async (actorId) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/person/${actorId}/images?api_key=${API_KEY}`
+    );
+    return response.data.profiles;
+  } catch (error) {
+    throw error;
+  }
+};
+
 const fetchMovieReviews = async (movieId) => {
   try {
     const response = await axios.get(
@@ -142,4 +75,5 @@ export {
   fetchMovieDetails,
   fetchMovieCredits,
   fetchMovieReviews,
+  fetchActorImages,
 };
