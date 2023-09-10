c633f4e5f9535eb1328ab483b36d7dd081513e6f
NickShuster <124299055+NickShuster@users.noreply.github.com>
2023-09-10 23:10:08 +0200

    ком

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,80 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import { fetchTrendingMovies, searchMovies } from '../api';
-
-// function Movies() {
-//   const [movies, setMovies] = useState([]);
-//   const [searchQuery, setSearchQuery] = useState('');
-//   const [searchResults, setSearchResults] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const trendingMovies = await fetchTrendingMovies();
-//         setMovies(trendingMovies);
-//       } catch (error) {
-//         console.error('Помилка під час завантаження популярних фільмів:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const handleSearchChange = (e) => {
-//     setSearchQuery(e.target.value);
-//   };
-
-//   const handleSearch = async () => {
-//     try {
-//       const results = await searchMovies(searchQuery);
-//       setSearchResults(results);
-//     } catch (error) {
-//       console.error('Помилка під час пошуку фільмів:', error);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <div>
-//         <input
-//           type="text"
-//           placeholder="Search for movies..."
-//           value={searchQuery}
-//           onChange={handleSearchChange}
-//         />
-//         <button onClick={handleSearch}>Search</button>
-//       </div>
-//       <ul>
-//         {searchResults.length > 0
-//           ? searchResults.map((movie) => (
-//               <li key={movie.id}>
-//                 <Link to={`/movies/${movie.id}?fromMovies=true`}>
-//                   {movie.title}
-//                 </Link>
-//               </li>
-//             ))
-//           : movies.map((movie) => (
-//               <li key={movie.id}>
-//                 <Link to={`/movies/${movie.id}?fromMovies=true`}>
-//                   {movie.title}
-//                 </Link>
-//               </li>
-//             ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default Movies;
-
-
-
-
-
-
-
-
-
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { searchMovies } from '../api';
@@ -90,13 +13,12 @@ function Movies() {
 
   const handleSearch = async () => {
     try {
-      // Очищаємо searchResults перед відправленням нового запиту
       setSearchResults([]);
       const results = await searchMovies(searchQuery);
       setSearchResults(results);
       setSearchedOnce(true);
 
-      // Зберігаємо результати пошуку в локальному сховищі браузера
+
       localStorage.setItem('searchResults', JSON.stringify(results));
     } catch (error) {
       console.error('Помилка під час пошуку фільмів:', error);
@@ -104,7 +26,8 @@ function Movies() {
   };
 
   useEffect(() => {
-    // Отримати результати пошуку з локального сховища браузера під час завантаження сторінки
+
+
     const savedSearchResults = JSON.parse(localStorage.getItem('searchResults'));
     if (savedSearchResults && savedSearchResults.length > 0) {
       setSearchResults(savedSearchResults);
@@ -118,6 +41,7 @@ function Movies() {
         <input
           type="text"
           placeholder="Search for movies..."
+          
           value={searchQuery}
           onChange={handleSearchChange}
         />
